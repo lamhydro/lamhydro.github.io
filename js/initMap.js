3ecf6216cc73559fcf0d10244b451625a3083188
Luis Morales <lmoralesma@gmail.com>
2019-11-07 21:43:22 -0600

    new website

---
+++ js/initMap.js
@@ -0,0 +1,11 @@
+
+// Initialize and add the map
+function initMap() {
+// The location of Uluru
+var uluru = {lat: 52.146973, lng: -106.647034};
+// The map, centered at Uluru
+var map = new google.maps.Map(
+  document.getElementById('map'), {zoom: 10, center: uluru});
+// The marker, positioned at Uluru
+var marker = new google.maps.Marker({position: uluru, map: map});
+}
