587bdeac07bb0be78ae161c845b180504eb19a4e
alejandro <lmoralesma@gmail.com>
2023-01-30 16:05:14 -0500

    updating the website, new name and articles

--- js/initMap.js
+++ js/initMap.js
@@ -1,11 +1,13 @@
 
 // Initialize and add the map
 function initMap() {
-// The location of Uluru
-var uluru = {lat: 52.146973, lng: -106.647034};
-// The map, centered at Uluru
-var map = new google.maps.Map(
+// The location of office
+const uluru = {lat: 45.483409, lng: -73.787903};
+// The map, centered at office 
+const map = new google.maps.Map(
   document.getElementById('map'), {zoom: 10, center: uluru});
-// The marker, positioned at Uluru
-var marker = new google.maps.Marker({position: uluru, map: map});
+// The marker, positioned at office
+const marker = new google.maps.Marker({position: uluru, map: map});
 }
+
+window.initMap = initMap;
